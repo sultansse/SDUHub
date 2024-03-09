262d16ba8b797adb6e026145442f6e5e7fc4b9e4
Sultan Suleimenov <ss.softwareit@gmail.com>
2024-03-09 13:07:48 +0500

    added mapbox again :(

--- app/src/main/java/com/softwareit/sduhub/ui/screens/MapFragment.kt
+++ app/src/main/java/com/softwareit/sduhub/ui/screens/MapFragment.kt
@@ -1,8 +1,6 @@
 package com.softwareit.sduhub.ui.screens
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -10,15 +8,18 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
+import com.mapbox.geojson.Point
+import com.mapbox.maps.CameraOptions
+import com.mapbox.maps.MapView
+import com.mapbox.maps.Style
 import com.softwareit.sduhub.R
 import com.softwareit.sduhub.base.BaseFragment
-import com.softwareit.sduhub.common.ui.WebViewWithLoading
+
 
 class MapFragment : BaseFragment() {
 
@@ -38,17 +39,25 @@ class MapFragment : BaseFragment() {
 
     @Composable
     fun MapScreen() {
-        val isLoading = remember { mutableStateOf(true) }
+        val LONGITUDE = 76.66967
+        val LATITUDE = 43.207176
 
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            WebViewWithLoading(
-                url = "https://app.mappedin.com/map/65e7640ca1cbc80d8a98ec9a/",
-                isLoading = isLoading
-            )
+        val context = LocalContext.current
+        val mapView = MapView(context)
+        AndroidView(
+            factory = { mapView },
+            modifier = Modifier.fillMaxSize()
+        ) { mapView ->
+            mapView.mapboxMap
+                .apply {
+                    loadStyle(Style.MAPBOX_STREETS)
+                    setCamera(
+                        CameraOptions.Builder()
+                            .center(Point.fromLngLat(LONGITUDE, LATITUDE))
+                            .zoom(16.0)
+                            .build()
+                    )
+                }
         }
     }
 
@@ -58,4 +67,3 @@ class MapFragment : BaseFragment() {
         SetContent()
     }
 }
-
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,6 +8,15 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        // Mapbox Maven repository
+        maven {
+            url = uri("https://api.mapbox.com/downloads/v2/releases/maven")
+            // Do not change the username below. It should always be "mapbox" (not your username).
+            credentials.username = "mapbox"
+            // Use the secret token stored in gradle.properties as the password
+            credentials.password = providers.gradleProperty("MAPBOX_DOWNLOADS_TOKEN").get()
+            authentication.create<BasicAuthentication>("basic")
+        }
 
         google()
         mavenCentral()
