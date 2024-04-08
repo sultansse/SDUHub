454f5374739d7669e684da87a9bb2dbea9f0b3c9
Sultan Suleimenov <ss.softwareit@gmail.com>
2024-04-08 13:14:16 +0500

    release/1.0.0-alpha02

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.softwareit.sduhub"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.0-alpha01"
+        versionCode = 2
+        versionName = "1.0.0-alpha02"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
